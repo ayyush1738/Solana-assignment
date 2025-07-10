f7c5d4f7de7a1d70556961eccd9c11237338bb18
unknown <singhrathoreayush824@gmail.com>
2025-07-10 21:03:57 +0530

    Ayush

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Navbar from '@/components/layout/Navbar';
-import HeroSection from '@/components/home/HeroSection';
-import FeaturesSection from '@/components/home/FeaturesSection';
-import Footer from '@/components/layout/Footer';
+import Navbar from '../components/layout/Navbar';
+import HeroSection from '../components/home/HeroSection';
+import FeaturesSection from '../components/home/FeaturesSection';
+import Footer from '../components/layout/Footer';
 
 export default function Home() {
   return (
